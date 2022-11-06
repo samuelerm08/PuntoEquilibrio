c71b856799dabe9ad82260f8b2fb8e05a025c06f
Samuel Rivera <108555378+samuelerm08@users.noreply.github.com>
2022-11-05 23:49:19 -0300

    Add files via upload

---
+++ Model/Barbijo.cs
@@ -0,0 +1,44 @@
+﻿namespace App.Model
+{
+    //Enumeracion para Estado
+    public enum Estado
+    {
+        Ganancia = 1,
+        Equilibrio = 0,
+        Perdida = -1
+    }
+    public class Barbijo
+    {        
+        public int codArt;
+        public string? nombre;
+        public double precioVenta;
+        public double costoFijo;
+        public double costoVariable;     
+        
+        public double PuntoEquilibrio()
+        {
+            return costoFijo / (precioVenta - costoVariable);
+
+        } 
+
+        public int CalculoEstado()
+        {
+            double puntoEquilibrio = PuntoEquilibrio();
+
+            if (puntoEquilibrio < 0)
+            {
+                return -1;
+            }
+
+            else if (puntoEquilibrio > 0)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }               
+    }
+}
---
+++ Model/Tabla.cs
@@ -0,0 +1,65 @@
+﻿namespace App.Model
+{
+    public class Tabla
+    {
+        private List<Barbijo> tabla = new List<Barbijo>();
+
+        public Tabla()
+        {
+            tabla.Add(new Barbijo()
+            {
+                codArt = 1,
+                nombre = "Barbijo de Tela",
+                precioVenta = 1000,
+                costoFijo = 100,
+                costoVariable = 500
+            }); 
+
+            tabla.Add(new Barbijo()
+            {
+                codArt = 2,
+                nombre = "Barbijo deportivo",
+                precioVenta = 500,
+                costoFijo = 6200,
+                costoVariable = 600
+            });
+
+            tabla.Add(new Barbijo()
+            {
+                codArt = 3,
+                nombre = "Barbijo Descartable",
+                precioVenta = 100,
+                costoFijo = 500,
+                costoVariable = 50
+            });
+
+            tabla.Add(new Barbijo()
+            {
+                codArt = 4,
+                nombre = "Barbijo de Tela Regulable",
+                precioVenta = 150,
+                costoFijo = 9000,
+                costoVariable = 500             
+            });
+
+            tabla.Add(new Barbijo()
+            {
+                codArt = 5,
+                nombre = "Barbijo Deportivo Regulable",
+                precioVenta = 1600,
+                costoFijo = 9000,
+                costoVariable = 1000               
+            });
+        }
+
+        public List<Barbijo> Show()
+        {
+            return tabla;
+        }
+
+        public Barbijo? ShowOne(int codArt)
+        {
+            return tabla.FirstOrDefault(barbijo => barbijo.codArt == codArt);
+        }
+    }
+}
